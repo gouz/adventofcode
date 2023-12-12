c4decf7bfee0afc6bba3b4f24d708a0e18b48f49
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-12-12 21:23:02 +0100

    feat: :sparkles: 202309 - Part 2

--- 2023/09/index.js
+++ 2023/09/index.js
@@ -40,3 +40,38 @@ const getPartOne = (text) => {
 
 console.log(getPartOne(test) === 114);
 console.log(`Part One: ${getPartOne(dataset)}`);
+
+const getExtraNumber = (line) => {
+  const numbers = line.split(" ").map((a) => Number(a));
+  const suits = [];
+  suits.push(numbers);
+  const all0 = (arr) => [...arr].filter((a) => a === 0).length === arr.length;
+
+  let diff;
+  let prev = [...numbers];
+  do {
+    diff = [];
+    for (let i = 1; i < prev.length; i++) {
+      diff.push(prev[i] - prev[i - 1]);
+    }
+    prev = [...diff];
+    suits.push(prev);
+  } while (!all0(diff));
+
+  // calc extra number
+  for (let j = suits.length - 2; j >= 0; j--) {
+    suits[j] = [suits[j][0] - suits[j + 1][0], ...suits[j]];
+  }
+  return suits[0].shift();
+};
+
+const getPartTwo = (text) => {
+  let sum = 0;
+  [...text.split("\n")].forEach((line) => {
+    sum += getExtraNumber(line);
+  });
+  return sum;
+};
+
+console.log(getPartTwo("10 13 16 21 30 45"));
+console.log(`Part Two: ${getPartTwo(dataset)}`);
