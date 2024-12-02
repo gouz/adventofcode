c5b7b15b46b15761f8579b84ead9666a22af43e2
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-12-02 23:55:14 +0100

    feat: :construction: day02part2

--- 2024/02.ts
+++ 2024/02.ts
@@ -2,44 +2,46 @@ const list = await Bun.file(`${process.cwd()}/2024/02/input.txt`).text();
 
 const parsed = list.split("\n").map((line) => line.split(" ").map(Number));
 
-const getDist = (report: number[]) => {
-	let direction = 0;
-	let dist = 0;
+const isSafe = (report: number[]) => {
+	const deltas: number[] = [];
 	for (let i = 1; i < report.length; i++) {
-		const delta = report[i] - report[i - 1];
-		const dir = delta >= 0 ? 1 : -1;
-		if (direction === 0) direction = dir;
-		if (direction !== dir || delta === 0) {
-			dist = 99;
-			break;
-		}
-		const d = Math.abs(delta);
-		if (d > dist) dist = d;
+		deltas.push(report[i] - report[i - 1]);
 	}
-	return dist;
+	const nbNeg = deltas.filter((n) => n < 0).length;
+	const nbPos = deltas.filter((n) => n > 0).length;
+	const nbNul = deltas.filter((n) => n === 0).length;
+	if (nbNul !== 0) return false;
+	if ((nbPos === 0 && nbNeg !== 0) || (nbPos !== 0 && nbNeg === 0)) {
+		return Math.max(...deltas.map(Math.abs)) <= 3;
+	}
+	return false;
 };
 
 let part1 = 0;
-parsed.forEach((report, _) => {
-	if (getDist(report) <= 3) part1++;
+parsed.forEach((report, i) => {
+	part1 += isSafe(report) ? 1 : 0;
 });
 console.log(part1);
 
+const cleanReport = (report: number[]) => {
+	const deltas: number[] = [];
+	for (let i = 1; i < report.length; i++) {
+		deltas.push(report[i] - report[i - 1]);
+	}
+	const nbNeg = deltas.filter((n) => n < 0).length;
+	const nbPos = deltas.filter((n) => n > 0).length;
+	const nbNul = deltas.filter((n) => n === 0).length;
+	if (nbNul === 1) return report.toSpliced(deltas.indexOf(0) + 1, 1);
+	if (nbNul === 0 && nbPos === 1)
+		return report.toSpliced(deltas.indexOf(1) + 1, 1);
+	if (nbNul === 0 && nbNeg === 1)
+		return report.toSpliced(deltas.indexOf(-1) + 1, 1);
+	return report;
+};
+
 let part2 = 0;
 parsed.forEach((report, _) => {
-	// clean
-	let direction = 0;
-	for (let i = 1; i < report.length; i++) {
-		const delta = report[i] - report[i - 1];
-		const dir = delta >= 0 ? 1 : -1;
-		if (direction === 0) direction = dir;
-		if (direction !== dir || delta === 0) {
-			report.splice(i, 1);
-			break;
-		}
-	}
-	if (getDist(report) <= 3) {
-		part2++;
-	}
+	const newReport = cleanReport(report);
+	part2 += isSafe(newReport) ? 1 : 0;
 });
 console.log(part2);
