5c8d564380bed968d04b44af1a5597cb52af1801
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-12-05 21:13:24 +0100

    feat: :construction: day 5

--- 2024/05/index.ts
+++ 2024/05/index.ts
@@ -2,34 +2,43 @@ import { loadInputFile } from "../../utils";
 
 const dataset = await loadInputFile("2024/05", "input");
 
-const before: { [key: string]: string[] } = {};
-const after: { [key: string]: string[] } = {};
+const hasAfter: { [key: string]: string[] } = {};
+const hasBefore: { [key: string]: string[] } = {};
 const sets: string[][] = [];
 
 dataset.split("\n").forEach((line, _) => {
 	if (line.includes("|")) {
 		const [from, to] = line.split("|");
-		if (!before[from]) before[from] = [];
-		before[from].push(to);
-		if (!after[to]) after[to] = [];
-		after[to].push(from);
+		if (!hasAfter[from]) hasAfter[from] = [];
+		hasAfter[from].push(to);
+		if (!hasBefore[to]) hasBefore[to] = [];
+		hasBefore[to].push(from);
 	} else if (line.includes(",")) {
 		sets.push(line.split(","));
 	}
 });
 
-const isCorrect = (set: string[]) => {
+const isCorrecAfter = (set: string[]) => {
 	const lSet = [...set];
-	const rSet = [...set];
 	for (let i = 0; i < set.length - 1; i++) {
 		const l = lSet.shift() || "";
-		const r = rSet.pop() || "";
-		if (!lSet.filter((value) => before[l]?.includes(value)).length)
+		if (!lSet.filter((value) => hasAfter[l]?.includes(value)).length)
 			return false;
-		if (!rSet.filter((value) => after[r]?.includes(value)).length) return false;
 	}
 	return true;
 };
+
+const isCorrectBefore = (set: string[]) => {
+	const rSet = [...set];
+	for (let i = 0; i < set.length - 1; i++) {
+		const r = rSet.pop() || "";
+		if (!rSet.filter((value) => hasBefore[r]?.includes(value)).length)
+			return false;
+	}
+	return true;
+};
+
+const isCorrect = (set: string[]) => isCorrecAfter(set) && isCorrectBefore(set);
 
 const getMiddle = (set: string[]) => set[Math.floor(set.length / 2)];
 
