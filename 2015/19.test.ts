70889fe3c24b4d4b4f26a8a13cdddb63b27f0e45
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-12-05 22:50:56 +0100

    2015/19 part1

--- 2015/19.test.ts
+++ 2015/19.test.ts
@@ -4,6 +4,6 @@ import { calc } from "./19";
 
 test("19", async () => {
   const input = calc(await loadInputFile("2015/19", "input"));
-  expect(input.part1).toBe(768);
+  expect(input.part1).toBe(535);
   expect(input.part2).toBe(781);
 });
--- 2015/19/index.ts
+++ 2015/19/index.ts
@@ -1,22 +1,20 @@
 export const calc = (input: string) => {
-  // const lines = input.split("\n").filter((l) => l !== "");
-  // const transforms = lines
-  //   .filter((l) => l.includes("=>"))
-  //   .map((l) => l.split("=>").map((t) => t.trim()));
-  // const formula = lines.pop();
-  const transforms = [
-    ["H", "HO"],
-    ["H", "OH"],
-    ["O", "HH"],
-  ];
-  const formula = "HOH";
-  const possibilities = new Set();
+  const lines = input.split("\n").filter((l) => l !== "");
+  const transforms = lines
+    .filter((l) => l.includes("=>"))
+    .map((l) => l.split("=>").map((t) => t.trim()));
+  const formula = lines.pop();
+  const possibilities: string[] = [];
   transforms.forEach(([from, to], _) => {
-    const occurences = (formula.match(new RegExp(from, "g")) || []).length;
-    for (let i = 0; i < occurences; i++) {
-      possibilities.add(formula?.replace(from, to));
+    const regexp = new RegExp(from, "g");
+    let matches: RegExpExecArray | null = null;
+    while ((matches = regexp.exec(formula)) !== null) {
+      possibilities.push(
+        matches.input.substring(0, matches.index) +
+          to +
+          matches.input.substring(matches.index + from.length),
+      );
     }
   });
-  console.log(possibilities);
-  return { part1: possibilities.size, part2: 0 };
+  return { part1: new Set(possibilities).size, part2: 0 };
 };
